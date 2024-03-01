c66aef0f3c7a10bfa45bdd1b8f26536b562af5ae
vinhn196 <vinhn96hn@gmail.com>
2024-03-01 14:14:06 +0700

    config_mui_theme

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,20 @@
-import createTheme from '@mui/material/styles'
+import { createTheme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({})
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6'
+      // main: '#f1f1f1'
+    },
+    secondary: {
+      main: '#19857b'
+    },
+    error: {
+      main: red.A400
+    }
+  }
+})
 
 export default theme
