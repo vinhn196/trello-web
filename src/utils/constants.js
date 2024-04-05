eaa2bf15ffc63df13d6f0d1af28386e6bd589215
vinhn196 <vinhn96hn@gmail.com>
2024-04-05 16:50:28 +0700

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT ='http://localhost:8017'
+
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev ') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production ') {
+  apiRoot = 'https://trello-api-v0es.onrender.com'
+}
+// export const API_ROOT ='http://localhost:8017'
+export const API_ROOT =apiRoot
