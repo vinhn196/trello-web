a9dd20adf58dce1d5868476f4982caa68fb299ca
vinhn196 <vinhn96hn@gmail.com>
2024-04-08 14:30:35 +0700

    deploy

--- src/apis/mock-data.js
+++ src/apis/mock-data.js
@@ -68,7 +68,7 @@ export const mockData = {
             FE_PlaceholderCard : true
           }
         ]
-      },
+      }
     ]
   }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,16 @@
 
 let apiRoot = ''
-if (process.env.BUILD_MODE === 'dev ') {
+
+console.log('import.meta.env: ', import.meta.env)
+console.log('process.env: ', process.env)
+
+if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
-if (process.env.BUILD_MODE === 'production ') {
+if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-api-v0es.onrender.com'
 }
+console.log('🚀 ~ apiRoot:', apiRoot)
+
 // export const API_ROOT ='http://localhost:8017'
 export const API_ROOT =apiRoot
